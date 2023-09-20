02e3c5f1f34365ccb6cc4c476eec13d93b6b2c54
Exe <exequielbaez@gmail.com>
2023-09-20 17:50:44 -0300

    Subir

---
+++ src/main/java/com/exe/applicationTask/ApplicationTaskApplication.java
@@ -0,0 +1,13 @@
+package com.exe.applicationTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe/applicationTask/ApplicationTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.applicationTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
